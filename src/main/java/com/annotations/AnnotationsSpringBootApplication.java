bc9c87985d0e5a802a86e146c42dff5dc803bc40
Vivek <ivegangwar@gmail.com>
2023-10-27 11:23:36 +0530

    First Commit

---
+++ src/main/java/com/annotations/AnnotationsSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.annotations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnotationsSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnnotationsSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/annotations/MyConfig.java
@@ -0,0 +1,8 @@
+package com.annotations;
+
+public class MyConfig {
+    void add()
+    {
+        System.out.println("Hello");
+    }
+}
